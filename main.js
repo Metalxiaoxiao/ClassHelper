ccc15b000be7b656b8bfcea25f5bc4cf924533e9
Metalxiaoxiao <58896585+Metalxiaoxiao@users.noreply.github.com>
2022-11-24 22:26:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+    win.loadFile('index.html')
+  };
+  app.whenReady().then(() => {
+    createWindow()
+  });
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
